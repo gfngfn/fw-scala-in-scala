5358f68579070ea818222def75dfed55d6063c43
Takashi SUWA <bd.gfngfn@gmail.com>
2019-05-10 00:14:44 +0900

    fix association as to '<~'

--- parser.scala
+++ parser.scala
@@ -7,36 +7,35 @@ object FWSParser extends JavaTokenParsers {
   def valueLabel :Parser[String] = ident
   def typeLabel : Parser[String] = ident
 
-  def expr : Parser[Any] = (
-    "val" ~> ((variable <~ "=") <~ "new") ~ (typ <~ ";") ~ expr
+  def expr : Parser[Any] =
+    ( "val" ~> ((variable <~ "=") <~ "new") ~ (typ <~ ";") ~ expr
     | exprChain
-  )
+    )
 
   def exprChain : Parser[Any] =
     variable ~ rep("." ~> valueLabel ~ opt(args))
 
   def args : Parser[Any] =
-    "(" ~ opt(rep1sep(expr, ",")) <~ ")"
+    "(" ~ (opt(rep1sep(expr, ",")) <~ ")")
 
   def params : Parser[Any] =
-    "(" ~ opt(rep1sep(expr <~ ":" ~ typ, ",")) <~ ")"
+    "(" ~ (opt(rep1sep((expr <~ ":") ~ typ, ",")) <~ ")")
 
-  def typ : Parser[Any] = (
-    singletonType | typeSignature | typeLabel
-  )
+  def typ : Parser[Any] =
+    ( singletonType | typeSignature | typeLabel )
 
   def singletonType : Parser[Any] =
     path <~ ".type"
 
   def typeSignature : Parser[Any] =
-    "(" ~> rep1sep(typ, ",") <~ ")" <~ "{" ~ variable <~ "|" ~ rep(decl) <~ "}"
+    "(" ~> ((rep1sep(typ, ",") <~ ")") <~ "{") ~ (variable <~ "|") ~ (rep(decl) <~ "}")
 
-  def decl : Parser[Any] = (
-    "val" ~> valueLabel <~ ":" ~ typ ~ opt("=" ~> expr)
-    | "def" ~> valueLabel <~ params <~ ":" ~ typ ~ opt("=" ~> expr)
+  def decl : Parser[Any] =
+    ( "val" ~> (valueLabel <~ ":") ~ typ ~ opt("=" ~> expr)
+    | "def" ~> valueLabel ~ (params <~ ":") ~ typ ~ opt("=" ~> expr)
     | "type" ~> typeLabel ~ opt("=" ~> typ)
     | "trait" ~> typeLabel ~ typeSignature
-  )
+    )
 
   def path : Parser[Any] =
     variable <~ rep("." ~> valueLabel)
