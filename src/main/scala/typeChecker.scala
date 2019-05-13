c82d146c5b3c8de30ccbe29ef8e1fea77f40faa6
Takashi SUWA <bd.gfngfn@gmail.com>
2019-05-13 22:46:08 +0900

    dev the type checker

--- src/main/scala/typeChecker.scala
+++ src/main/scala/typeChecker.scala
@@ -3,10 +3,23 @@ sealed trait TypeError
 case class TypeNotAFieldButAMethod(vl : String)           extends TypeError
 case class TypeNotAMethodButAField(vl : String)           extends TypeError
 case class TypeWrongNumberOfArguments(la : Int, lp : Int) extends TypeError
+case class VariableExtrudesItsScope(x : String, ty : Type) extends TypeError
 
 
 class Store {}  // TEMPORARY
-class TypeEnv {}  // TEMPORARY
+
+trait TypeEnv {
+
+  val body : Map[String, Type]
+
+  def add(x : String, ty : Type) : TypeEnv =
+    new TypeEnv { val body = TypeEnv.this.body + ((x, ty)) }
+
+}
+
+class EmptyTypeEnv extends TypeEnv {
+  val body = Map()
+}
 
 
 object FWSTypeChecker {
@@ -52,7 +65,19 @@ object FWSTypeChecker {
         }
 
       case ValNewIn(x, ty, ast0) =>
-        ???
+        checkTypeWellFormedness(store, tyenv, ty) flatMap { _ =>
+          typeCheck(store, tyenv.add(x, ty), ast0) flatMap { ty0 =>
+            if (occursInType(x, ty0)) {
+              Left(VariableExtrudesItsScope(x, ty0))
+            } else {
+              val phi : String = "(dummy)";
+                /* -- a dummy variable name -- */
+              expandType(store, tyenv, phi, ty) flatMap { _ =>
+                Right(ty0)
+              }
+            }
+          }
+        }
 
     }
 
@@ -76,12 +101,37 @@ object FWSTypeChecker {
   }
 
 
+  def occursInType(x : String, ty : Type) : Boolean =
+    ???
+
+
+  def expandType(store : Store, tyenv : TypeEnv, x : String, ty : Type) : Either[TypeError, List[Declaration]] =
+    ???
+
+
+  def checkTypeWellFormedness(store : Store, tyenv :TypeEnv, ty : Type) : Either[TypeError, Unit] =
+    ???
+
+
   def isSubtype(store : Store, tyenv : TypeEnv, ty1 : Type, ty2 : Type) : Either[TypeError, Unit] =
     ???
 
 
-  def pathOfAstIfPossible(ast : Ast) : Option[Path] =
-    ???
+  def pathOfAstIfPossible(ast : Ast) : Option[Path] = {
+    def aux(vlabelacc : List[String], ast : Ast) : Option[Path] = {
+      ast match {
+        case Access(ast0, vlabel) =>
+          aux(vlabel :: vlabelacc, ast0)
+
+        case Var(x) =>
+          Some(Path(x, vlabelacc.reverse))
+
+        case _ =>
+          None
+      }
+    };
+    aux(Nil, ast)
+  }
 
 
   def typeCheckPath(store : Store, tyenv : TypeEnv, path : Path) : Either[TypeError, Type] =
