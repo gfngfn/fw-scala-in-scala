2d35e330c0b3cfb9597e34821abd3a9ecdbd1d61
Takashi SUWA <bd.gfngfn@gmail.com>
2019-05-15 12:51:06 +0900

    dev the type checker

--- src/main/scala/typeChecker.scala
+++ src/main/scala/typeChecker.scala
@@ -1,13 +1,31 @@
 
+trait DeclarationID {
+
+  val body : Option[Int]
+
+}  // TEMPORARY
+
+
 sealed trait TypeError
 case class TypeNotAFieldButAMethod(vl : String)            extends TypeError
 case class TypeNotAMethodButAField(vl : String)            extends TypeError
 case class TypeWrongNumberOfArguments(la : Int, lp : Int)  extends TypeError
 case class VariableExtrudesItsScope(x : String, ty : Type) extends TypeError
 case class UndefinedVariable(x : String)                   extends TypeError
+case class AlreadySeen(did : DeclarationID)                extends TypeError
 
 
-class Store {}  // TEMPORARY
+trait Store {
+
+  def contains(did : DeclarationID) : Boolean =
+    ???
+
+
+  def add(did : DeclarationID) : Store =
+    ???
+
+}  // TEMPORARY
+
 
 trait TypeEnv {
 
@@ -28,6 +46,10 @@ class EmptyTypeEnv extends TypeEnv {
 
 
 object FWSTypeChecker {
+
+  def generateFreshVariable() : String =
+    ???
+
 
   def typeCheck(store : Store, tyenv : TypeEnv, ast : Ast) : Either[TypeError, Type] =
     ast match {
@@ -75,7 +97,7 @@ object FWSTypeChecker {
             if (occursInType(x, ty0)) {
               Left(VariableExtrudesItsScope(x, ty0))
             } else {
-              val phi : String = "(dummy)";
+              val phi : String = generateFreshVariable();
                 /* -- a dummy variable name -- */
               expandType(store, tyenv, phi, ty) flatMap { _ =>
                 Right(ty0)
@@ -134,24 +156,65 @@ object FWSTypeChecker {
   }
 
 
-  def typeCheckPath(store : Store, tyenv : TypeEnv, path : Path) : Either[TypeError, Type] =
+  def separateLastLabel(path : Path) : Either[String, (Path, String)] =
     path match { case Path(x, vlabels) =>
       vlabels.reverse match {
-        case Nil =>
-          tyenv.findVariable(x) match {
-            case None     => Left(UndefinedVariable(x))
-            case Some(ty) => Right(ty)
-          }
-
-        case vlabelLast :: vlabelRevTail =>
-          membership(store, tyenv, SingletonType(Path(x, vlabelRevTail.reverse)), vlabelLast) flatMap {
-            case DeclVal(ty, _)   => Right(ty)
-            case DeclDef(_, _, _) => Left(TypeNotAFieldButAMethod(vlabelLast))
-          }
+        case Nil                   => Left(x)
+        case vlabelLast :: revTail => Right((Path(x, revTail.reverse), vlabelLast))
       }
     }
 
 
-  def membership(store : Store, tyenv : TypeEnv, ty : Type, vlabel : String) : Either[TypeError, ValueDeclBody] =
+  def typeCheckPath(store : Store, tyenv : TypeEnv, path : Path) : Either[TypeError, Type] =
+    separateLastLabel(path) match {
+      case Left(x) =>
+        tyenv.findVariable(x) match {
+          case None     => Left(UndefinedVariable(x))
+          case Some(ty) => Right(ty)
+        }
+
+      case Right((pathRest, vlabelLast)) =>
+        membership(store, tyenv, SingletonType(pathRest), vlabelLast) flatMap {
+          case DeclVal(ty, _)   => Right(ty)
+          case DeclDef(_, _, _) => Left(TypeNotAFieldButAMethod(vlabelLast))
+        }
+    }
+
+
+  def membership(store : Store, tyenv : TypeEnv, ty : Type, vlabel : String) : Either[TypeError, ValueDeclBody] = {
+    val phi : String = generateFreshVariable();
+    ty match {
+      case SingletonType(pathP) =>
+        expandPathAlias(store, tyenv, pathP) flatMap { case (pathQ, tyT) =>
+          getDeclarationID(store, tyenv, pathP) flatMap { did =>
+            if (store.contains(did)) {
+              Left(AlreadySeen(did))
+            } else {
+              expandType(store.add(did), tyenv, phi, tyT) flatMap { decls =>
+                ???
+              }
+            }
+          }
+        }
+
+      case _ =>
+        expandType(store, tyenv, phi, ty) flatMap { decls =>
+          ???
+        }
+    }
+  }
+
+
+  def getDeclarationID(store : Store, tyenv : TypeEnv, path : Path) : Either[TypeError, DeclarationID] =
+    separateLastLabel(path) match {
+      case Left(x) =>
+        ???
+
+      case Right((pathP, vlabel)) =>
+        ???
+    }
+
+
+  def expandPathAlias(store : Store, tyenv : TypeEnv, path : Path) : Either[TypeError, (Path, Type)] =
     ???
 }
