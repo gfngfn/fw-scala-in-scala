8c94bd4481108f3bdefd90fb5b2c946c3f3afe01
gfngfn <suwa-takashi141@g.ecc.u-tokyo.ac.jp>
2019-08-24 15:49:35 +0900

    connect the type checker to the main CLI

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -7,8 +7,18 @@ object FWS {
         val parseResult : FWSParser.ParseResult[Ast] = FWSParser.parseAll(FWSParser.expr, code);
         val ast : Ast = parseResult.get;
         println("parsed result: " + ast);
-        val interp = new FWSInterpreter;
-        interp.run(ast)
+        val store = new EmptyStore{}
+        val tyenv = new EmptyTypeEnv{}
+        val res : Either[TypeError, Type] = FWSTypeChecker.typeCheck(store, tyenv, ast)
+        res match {
+          case Left(err) =>
+            println("type error: " + err)
+
+          case Right(ty) =>
+            println("type: " + ty)
+            val interp = new FWSInterpreter
+            interp.run(ast)
+        }
 
       case _ =>
         println("wrong number of arguments; required one argument")
